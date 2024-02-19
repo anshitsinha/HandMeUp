b173216973a8ec1a78c52ac51e88d23779764e48
anshitsinha <anshit81_soe@jnu.ac.in>
2024-02-19 17:57:20 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-        HandMeUp
+        <Link to = "/"> HandMeUp </Link>
         <Link to = "/login"> LOGIN </Link>
     </div>
   )
