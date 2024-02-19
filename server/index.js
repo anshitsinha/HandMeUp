955f6a0a20a88cd3b879b962219855c94001184e
anshitsinha <anshit81_soe@jnu.ac.in>
2024-02-19 18:49:24 +0530

    user api working

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
-const PORT = 3000
+
+const PORT = 4000
+mongoose.connect('mongodb+srv://anshitsinha:G2EFw5bCR9L7SwVq@cluster0.9vn34dg.mongodb.net/');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const Users = mongoose.model('Users', userSchema);
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/signup', (req,res) => {
+    // const username = req.body.username;
+    // const password = req.body.password;
+    const user = new Users({
+        username: 'username',
+        password: 'password'
+    });
+    user.save().then(() => console.log('saved'));
 })
 
 app.listen(PORT, () => {
