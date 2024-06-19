4549ccc63d84ad8aff23e1a69204d4d042737f39
anshitsinha <anshit81_soe@jnu.ac.in>
2024-06-19 15:10:05 +0530

    bug fix sell

--- src/app/add-product/page.jsx
+++ src/app/add-product/page.jsx
@@ -6,7 +6,7 @@ import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import AddProductForm from "@src/components/AddProductForm";
 
-export default function sell() {
+export default function Sell() {
   const session = useSession({
     required: true,
     onUnauthenticated() {
